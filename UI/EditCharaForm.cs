80514ca975408c9350372b8d9b25baedefc88b91
YukkuriC <799433638@qq.com>
2022-12-11 23:54:35 +0800

    角色编辑页无角色时判空

--- UI/EditCharaForm.cs
+++ UI/EditCharaForm.cs
@@ -14,7 +14,7 @@ namespace ChusanExplorer
             chara = character;
             profileInput = profile;
             profileEdit = profile?.Clone();
-            if (profileEdit.maxLevel == 0) profileEdit.maxLevel = 15;
+            if (profileEdit?.maxLevel == 0) profileEdit.maxLevel = 15;
             flushDisplay();
         }
 
